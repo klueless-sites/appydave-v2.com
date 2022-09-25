00eac4f94fb43a84279ceab1081a1b2e2ffefed2
David Cruwys <david@ideasmen.com.au>
2022-09-26 09:13:00 +1000

    update

---
+++ src/pages/ruby-gems/factory-bot/nav.ts
@@ -0,0 +1,16 @@
+export default [
+  {
+    title: 'Introduction',
+    links: [
+      { title: 'Getting started', href: '/' },
+      { title: 'Installation', href: '/docs/installation' },
+    ],
+  },
+  {
+    title: 'Test',
+    links: [
+      { title: 'Getting started', href: '/' },
+      { title: 'Installation', href: '/docs/installation' },
+    ],
+  },
+] as const;
